5fca4e6569a942c0c4bb754aa33e71b0c9a38699
projblacktrack <projblacktrack@gmail.com>
2026-08-23 00:46:35 +0530

    Refactor fetch_global_wildfire_data function
    Removed docstring from fetch_global_wildfire_data function and adjusted indentation for consistency.

--- source/api/dependencies/data_fetcher.py
+++ source/api/dependencies/data_fetcher.py
@@ -5,23 +5,17 @@ import h3
 
 
 def fetch_global_widfire_data():
-    """
-    Fetches global wildfire data from the specified URL and returns it as a pandas DataFrame.
-    
-    Returns:
-        pd.DataFrame: A DataFrame containing the global wildfire data.
-    """
     url = f"https://firms.modaps.eosdis.nasa.gov/api/area/csv/{settings.MAP_KEY}/VIIRS_NOAA20_NRT/world/1"
     try:
         df = pd.read_csv(url)
-          df['latitude'] = df['latitude'].astype(float)
-          df['longitude'] = df['longitude'].astype(float)
-          df['h3_index'] = [h3.latlng_to_cell(lat, lng, resolution) for lat, lng in zip(df['latitude'], df['longitude'])]
-          df = df.drop_duplicates(subset=['h3_index'])
+        df['latitude'] = df['latitude'].astype(float)
+        df['longitude'] = df['longitude'].astype(float)
+        df['h3_index'] = [h3.latlng_to_cell(lat, lng, resolution) for lat, lng in zip(df['latitude'], df['longitude'])]
+        df = df.drop_duplicates(subset=['h3_index'])
         
-          df = df[df['frp'] > 17]
-          json_df = df.to_json(orient="records")
-          return json_df
+        df = df[df['frp'] > 17]
+        json_df = df.to_json(orient="records")
+        return json_df
     except Exception as e:
         print(f"Error fetching data from {url}: {e}")
         return pd.DataFrame()  
